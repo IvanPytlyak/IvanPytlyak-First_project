0cd89a2dffa1ddfdd1602673c19b005837744b84
Ivan Pytlyak <mario38671@gmail.com>
2022-10-29 23:51:04 +0300

    29/10-4

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let newButton = document.querySelector('.button1');
-newButton.addEventListener('click', function() {
-    newButton.style.borderColor='#e71414';
+
+
+let BurderOpen = document.getElementById('burgermenu');
+BurderOpen.addEventListener('click', function(event){
+    if (BurderOpen.classList == 'open'){
+        BurderOpen.classList.remove('open');
+        console.log('1');
+        event.stopPropagation();
+    }
+    else {
+        BurderOpen.classList.add('open');
+        console.log('2');
+        event.stopPropagation();
+    } 
 })
+
+// BurderOpen.addEventListener('click', function(){
+//     BurderOpen.classList.toggle('open');
+// })
